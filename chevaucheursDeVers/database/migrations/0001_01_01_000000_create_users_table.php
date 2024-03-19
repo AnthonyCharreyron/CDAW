13b31ae4fe8685f87b4ef09de88572f965511e69
Charreyron Anthony <charreyronanthony0263@gmail.com>
2024-03-19 17:54:53 +0100

    Début bdd

--- chevaucheursDeVers/database/migrations/0001_01_01_000000_create_users_table.php
+++ chevaucheursDeVers/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('pseudo', 100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('mot_de_passe');
             $table->rememberToken();
             $table->timestamps();
+            $table->boolean('est_bloque')->default(false);
+            $table->boolean('est_administrateur')->default(false);
+            $table->integer('photo_profil')->default(0);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
--- chevaucheursDeVers/database/seeders/UserSeeder.php
+++ chevaucheursDeVers/database/seeders/UserSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 
-class UtilisateurSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,8 +15,8 @@ class UtilisateurSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('utilisateurs')->insert([
-            ['pseudo'=>'Tom', 'mail'=>'tom@free.fr', 'mot_de_passe'=>'tom', 'est_bloque'=>0, 'photo_profil'=>0, 'est_administrateur'=>0],
+        DB::table('users')->insert([
+            ['id'=>1, 'pseudo'=>'Tom', 'email'=>'tom@free.fr', 'mot_de_passe'=>'tom', 'est_bloque'=>0, 'photo_profil'=>0, 'est_administrateur'=>0],
             ['pseudo'=>'Riri', 'mail'=>'riri@free.fr', 'mot_de_passe'=>'riri', 'est_bloque'=>1, 'photo_profil'=>1, 'est_administrateur'=>0],
             ['pseudo'=>'Fifi', 'mail'=>'fifi@free.fr', 'mot_de_passe'=>'fifi', 'est_bloque'=>0, 'photo_profil'=>0, 'est_administrateur'=>1]
         ]);
