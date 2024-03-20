01c60d4cb5a99e5b5701077f0fb6f50f2ea8f18d
Charreyron Anthony <charreyronanthony0263@gmail.com>
2024-03-20 08:40:29 +0100

    Début seeder

--- chevaucheursDeVers/database/seeders/UserSeeder.php
+++ chevaucheursDeVers/database/seeders/UserSeeder.php
@@ -16,9 +16,28 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            ['id'=>1, 'pseudo'=>'Tom', 'email'=>'tom@free.fr', 'mot_de_passe'=>'tom', 'est_bloque'=>0, 'photo_profil'=>0, 'est_administrateur'=>0],
-            ['pseudo'=>'Riri', 'mail'=>'riri@free.fr', 'mot_de_passe'=>'riri', 'est_bloque'=>1, 'photo_profil'=>1, 'est_administrateur'=>0],
-            ['pseudo'=>'Fifi', 'mail'=>'fifi@free.fr', 'mot_de_passe'=>'fifi', 'est_bloque'=>0, 'photo_profil'=>0, 'est_administrateur'=>1]
+            'pseudo' => 'admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'mot_de_passe' => Hash::make('password'),
+            'est_administrateur' => true,
+            'est_bloque' => false,
+            'photo_profil' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'pseudo' => 'utilisateur' . $i,
+                'email' => 'user' . $i . '@example.com',
+                'email_verified_at' => now(),
+                'mot_de_passe' => Hash::make('password'),
+                'est_administrateur' => false,
+                'est_bloque' => false,
+                'photo_profil' => 0,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
