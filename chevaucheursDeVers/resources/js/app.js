0b322a810407f5e6d2b9bca3142c5727f5fa9053
Anna Ruiz <anna.ruiz@free.fr>
2024-04-02 12:00:29 +0200

    front de la page présentation terminé

--- chevaucheursDeVers/resources/js/app.js
+++ chevaucheursDeVers/resources/js/app.js
@@ -8,3 +8,12 @@ import './lobby';
 import './gestionUser';
 import './partie';
 
+jQuery(function($){
+    $(document).ready(function() {
+        $('#btn-regles-jeu').on('click', function() {
+            $('#regles-jeu').toggle();
+        });
+    });
+})
+
+
