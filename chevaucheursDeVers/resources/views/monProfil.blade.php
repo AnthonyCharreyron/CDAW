3de06c00dcb161a271dc440268fb441f1acae7b2
Anna Ruiz <anna.ruiz@free.fr>
2024-04-04 00:45:59 +0200

    début show profil lorsqu'on passe sur un profil d'ami

--- chevaucheursDeVers/resources/views/monProfil.blade.php
+++ chevaucheursDeVers/resources/views/monProfil.blade.php
@@ -70,6 +70,35 @@
                     </tr>
                 </thead>
             </table>
+            <!-- Bouton pour voir le profil -->
+            <!-- <button type="button" class="btn btn-primary btn-voir-profil" data-id-ami="ID_AMI_AFFICHÉ"  data-bs-toggle="modal" data-bs-target="#modalProfilAmi">Voir le profil</button> -->
+            <img src="{{asset('images/1.png')}}" style="height: 5vh;"  alt="Voir le profil" class="img-voir-profil" data-id-ami="ID_AMI_AFFICHÉ">
+            <!-- Modal pour afficher les informations de l'ami -->
+
+            <div class="modal fade" id="modalProfilAmi" tabindex="-1" aria-labelledby="modalProfilAmiLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalProfilAmiLabel">Profil de <span id="amiPseudo"></span></h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img id="amiPhotoProfil" src="{{asset('images/1.png')}}" alt="Photo de profil" class="img-fluid rounded">
+                                </div>
+                                <div class="col-md-8">
+                                    <p><strong>Pseudo :</strong> <span id="amiPseudoModal"></span></p>
+                                    <p><strong>Parties jouées :</strong> <span id="amiPartiesJouees"></span></p>
+                                    <p><strong>Parties gagnées :</strong> <span id="amiPartiesGagnees"></span></p>
+                                    <p><strong>Meilleur score :</strong> <span id="amiMeilleurScore"></span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
